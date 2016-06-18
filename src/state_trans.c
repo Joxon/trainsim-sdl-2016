2725a45de218be1994f8c4cce38c29d9f84a5c0b
Joxon Chan <cijophxon@gmail.com>
2016-06-19 00:01:14 +0800

    bug修复仍然爆炸，反向会撞车

--- src/state_trans.c
+++ src/state_trans.c
@@ -243,7 +243,7 @@ void changePosition(struct train *tra)
 int judgeCommonTrack(struct train *tra, struct block rail[][MAX_RAIL_LENGTH], int i)
 {
 	unsigned int j, x = 0, y = 0;
-	unsigned int pos=tra->position,posi;
+	int pos=tra->position,posi;
 
 	//在探测点判断公共轨道是否有车
 	//顺时针
@@ -253,11 +253,11 @@ int judgeCommonTrack(struct train *tra, struct block rail[][MAX_RAIL_LENGTH], in
 		if (tra->speed == 0 && tra->status == PAUSE_COMMON)
 		{
 			//当前秒
-			pos += trainSpeed[i];
+			pos += (int)trainSpeed[i];
 			if(pos >= tra->railwayLength)
 				pos = pos+ tra->railwayLength;
 			for (j = 0; j < trainNum; j++)
-				if (railway[j][train[j].position].common
+				if (railway[j][trains[j].position].common
 					== rail[i][pos].common)
 					x++;
 			//下一秒
@@ -265,13 +265,13 @@ int judgeCommonTrack(struct train *tra, struct block rail[][MAX_RAIL_LENGTH], in
 				for (j = 0; j < trainNum; j++)
 					if (trains[j].speed == 0 && trains[j].status == PAUSE_COMMON&&strategy != 3)
 					{
-						if (railway[j][train[j].position + (int)trainSpeed[j]].common
+						if (railway[j][trains[j].position + (int)trainSpeed[j]].common
 							== rail[i][pos].common)
 							y++;
 						if (j != i)
 							m = j;
 					}
-					else if (railway[j][train[j].position + (int)train[j].speed].common
+					else if (railway[j][trains[j].position + (int)trains[j].speed].common
 						== rail[i][pos].common) {
 						y++;
 						if (j != i)
@@ -286,7 +286,7 @@ int judgeCommonTrack(struct train *tra, struct block rail[][MAX_RAIL_LENGTH], in
 			if (pos >= tra->railwayLength)
 				pos = pos - tra->railwayLength;
 			for (j = 0; j < trainNum; j++)
-				if (railway[j][train[j].position].common
+				if (railway[j][trains[j].position].common
 					== rail[i][pos].common)
 					x++;
 			//下一秒
@@ -294,13 +294,13 @@ int judgeCommonTrack(struct train *tra, struct block rail[][MAX_RAIL_LENGTH], in
 				for (j = 0; j < trainNum; j++)
 					if (trains[j].speed == 0 && trains[j].status == PAUSE_COMMON&&strategy != 3)
 					{
-						if (railway[j][train[j].position + (int)trainSpeed[j]].common
+						if (railway[j][trains[j].position + (int)trainSpeed[j]].common
 							== rail[i][pos].common)
 							y++;
 						if (j != i)
 							m = j;
 					}
-					else if (railway[j][train[j].position + (int)train[j].speed].common
+					else if (railway[j][trains[j].position + (int)trains[j].speed].common
 						== rail[i][pos].common) {
 						y++;
 						if (j != i)
@@ -316,23 +316,23 @@ int judgeCommonTrack(struct train *tra, struct block rail[][MAX_RAIL_LENGTH], in
 		//在公共轨道前自动暂停的情况
 		if (tra->speed == 0 && tra->status == PAUSE_COMMON)
 		{
-			pos -= trainSpeed[i];
+			pos -= (int)trainSpeed[i];
 			if (pos <0)
 				pos = pos + tra->railwayLength+1;
 			//当前秒
 			for (j = 0; j < trainNum; j++)
-				if (railway[j][train[j].position].common
+				if (railway[j][trains[j].position].common
 					== rail[i][pos].common)
 					x++;
 			//下一秒
 			if (x == 0)
 				for (j = 0; j < trainNum; j++)
-					if (train[j].speed == 0 && train[j].status == PAUSE_COMMON&&strategy != 3)
+					if (trains[j].speed == 0 && trains[j].status == PAUSE_COMMON&&strategy != 3)
 					{   
-						if (train[j].position - (int)trainSpeed[j] > 0)
-							posi = train[j].position - (int)trainSpeed[j];
+						if (trains[j].position - (int)trainSpeed[j] > 0)
+							posi = trains[j].position - (int)trainSpeed[j];
 						else
-							posi = train[j].position - (int)trainSpeed[j] + tra->railwayLength;
+							posi = trains[j].position - (int)trainSpeed[j] + tra->railwayLength;
 						if (railway[j][posi].common
 							== rail[i][pos].common)
 							y++;
@@ -340,11 +340,11 @@ int judgeCommonTrack(struct train *tra, struct block rail[][MAX_RAIL_LENGTH], in
 							m = j;
 					}
 					else {
-						if (train[j].position - (int)trainSpeed[j] > 0)
-							posi = train[j].position - (int)train[j].speed;
+						if (trains[j].position - (int)trainSpeed[j] > 0)
+							posi = trains[j].position - (int)trains[j].speed;
 						else
-							posi = train[j].position - (int)train[j].speed + tra->railwayLength;
-						if (railway[j][train[j].position - (int)train[j].speed].common
+							posi = trains[j].position - (int)trains[j].speed + tra->railwayLength;
+						if (railway[j][trains[j].position - (int)trains[j].speed].common
 						== rail[i][pos].common) {
 						y++;
 						if (j != i)
@@ -360,7 +360,7 @@ int judgeCommonTrack(struct train *tra, struct block rail[][MAX_RAIL_LENGTH], in
 				pos = pos + tra->railwayLength+1;
 			//当前秒
 			for (j = 0; j < trainNum; j++)
-				if (railway[j][train[j].position].common
+				if (railway[j][trains[j].position].common
 					== rail[i][pos].common)
 					x++;
 			//下一秒
@@ -368,10 +368,10 @@ int judgeCommonTrack(struct train *tra, struct block rail[][MAX_RAIL_LENGTH], in
 				for (j = 0; j < trainNum; j++)
 					if (trains[j].speed == 0 && trains[j].status == PAUSE_COMMON&&strategy != 3)
 					{
-						if (train[j].position - (int)trainSpeed[j] > 0)
-							posi = train[j].position - (int)trainSpeed[j];
+						if (trains[j].position - (int)trainSpeed[j] > 0)
+							posi = trains[j].position - (int)trainSpeed[j];
 						else
-							posi = train[j].position - (int)trainSpeed[j] + tra->railwayLength;
+							posi = trains[j].position - (int)trainSpeed[j] + tra->railwayLength;
 						if (railway[j][posi].common
 							== rail[i][pos].common)
 							y++;
@@ -379,10 +379,10 @@ int judgeCommonTrack(struct train *tra, struct block rail[][MAX_RAIL_LENGTH], in
 							m = j;
 					}
 					else{
-						if (train[j].position - (int)trainSpeed[j] > 0)
-							posi = train[j].position - (int)train[j].speed;
+						if (trains[j].position - (int)trainSpeed[j] > 0)
+							posi = trains[j].position - (int)trains[j].speed;
 						else
-							posi = train[j].position - (int)train[j].speed + tra->railwayLength;
+							posi = trains[j].position - (int)trains[j].speed + tra->railwayLength;
 						if (railway[j][posi].common
 						== rail[i][pos].common) {
 						y++;
